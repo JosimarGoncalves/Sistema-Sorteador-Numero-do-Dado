5a1c5e06a2abeccfbed9e7f5a0459e2ac8c5d633
Josimar Goncalves <foxchanger@hotmail.com>
2021-12-20 01:24:41 -0300

    Upd Code

--- simulado_de_dados.py
+++ simulado_de_dados.py
@@ -7,37 +7,34 @@ class SimuladorDeDado:
         self.valor_minimo = 1
         self.valor_maximo = 6
         self.mensagem ='Vocẽ gostaria de gerar um novo valor para o o dado'
-        
-        
-        
-
-
-
-    def Iniciar(self):
-        
         #Layout
-        layout =[
+        self.layout =[
             [sg.Text('Jogar o dado')],
             [sg.Button('sim')],[sg.Button('nao')]
         ]
+
+    def Iniciar(self):   
         
         #Criar uma Janela
-        self.janela = sg.Window('Simulador de Dado',Layout=layout)
+        ##window = sg.Window('Simulador de Dado', layout)
+        self.janela = sg.Window('Window Title',layout=self.layout)
         
         #Ler os Arquivos da Tela
         self.eventos, self.valores = self.janela.Read()
         
-        #Fazer alguma coisa com esses valores 
-        while True:
-            try:
-                if self.eventos == 'sim' or self.eventos =='s':
-                    self.GerarValorDoDado()
-                elif self.eventos == 'nao' or self.eventos == 'n':
-                    print('Agradecemos a sua participação!')
-                else:
-                    print('Favor digitar sim ou nao')   
-            except:
-                print('Ocorreu um erro ao receber sua resposta!')            
+        #Fazer alguma coisa com esses valores
+        try:
+            if self.eventos == 'sim' or self.eventos =='s':
+                self.GerarValorDoDado()
+            elif self.eventos == 'nao' or self.eventos == 'n':
+                print('Agradecemos a sua participação!')
+            else:
+                print('Favor digitar sim ou nao')
+        except:
+            print('Ocorreu um erro ao receber sua resposta!') 
+        
+            
+                    
             
           
             
