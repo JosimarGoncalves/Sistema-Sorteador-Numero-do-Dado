de35ac264a748a7399e3e547fadb19e1afcbde19
Josimar Goncalves <foxchanger@hotmail.com>
2021-12-19 22:25:28 -0300

    Upd COde

--- simulado_de_dados.py
+++ simulado_de_dados.py
@@ -1,1 +1,19 @@
 # Simulador de dado
+import random
+
+class SimuladorDeDado:
+    def __init__(self):
+        self.valor_minimo = 1
+        self.valor_maximo = 6
+        self.mensagem ='Vocẽ gostaria de gerar um novo valor para o o dado'
+
+    def Iniciar(self):
+        resposta = input(self.mensagem)
+        if resposta == 'sim':
+            self.GerarValorDoDado()
+
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo,self.valor_maximo))
+
+simular = SimuladorDeDado()
+simular.Iniciar()                
